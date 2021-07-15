411d372d7660544c04fe14060e052c8af727dcfb
Tamil Selvan V <tamilselvan.v@razorpay.com>
2021-07-15 15:25:16 +0530

    added swagger for get all rows api

--- restify.py
+++ restify.py
@@ -1,10 +1,11 @@
 from logging import getLogger
 
+from flasgger import Swagger
 from sqlalchemy import MetaData
 
 from config import ConfigReader
 from connection import DBConnection
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from db_service import DBService
 from filter import Filters
@@ -13,6 +14,11 @@ logger = getLogger(__name__)
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['SWAGGER'] = {
+    'title': 'RestifyDB',
+    'uiversion': 3
+}
+swagger = Swagger(app)
 
 config = ConfigReader(filepath="sample.json").config
 db_connection = DBConnection(**config["connection_params"])
@@ -34,6 +40,60 @@ def fetch_all_tables():
 
 @app.route("/tables/<table_name>", methods=["GET"])
 def fetch_all_rows(table_name: str):
+    """
+    Return paginated response of all the rows from the table
+    ---
+    parameters:
+        - name: table_name
+          in: path
+          type: string
+          required: true
+          description: name of the table
+        - name: pageNo
+          in: query
+          type: string
+          required: false
+          description: page number for paginated response
+          default: 1
+        - name: maxRows
+          in: query
+          type: string
+          required: false
+          description: max number of rows per page in the response
+          default: 100
+        - in: query
+          name: params
+          description: |
+            Filter parameters (key=value)
+                key - column names suffixed which operator
+                eg. id__eq => apply "==" on column 'id'
+                    temp__gt => apply ">" on column 'temp'
+                Currently Supported Operators:
+                - "eq" => "=="
+                - "gt" => ">"
+                - "lt" => "<"
+                - "ge" => ">="
+                - "le" => "<="
+
+            value - value for the column that should be applied for the filter
+          schema:
+            type: object
+            # If the parameter values are of specific type, e.g. string:
+            additionalProperties:
+              type: string
+            # If the parameter values can be of different types
+            # (e.g. string, number, boolean, ...)
+            # additionalProperties: true
+
+          # `style: form` and `explode: true` is the default serialization method
+          # for query parameters, so these keywords can be omitted
+          style: form
+          explode: true
+
+    responses:
+      200:
+        description: A list of rows (may be filtered by args, pageNo, maxRows)
+    """
     meta = MetaData()
     meta.reflect(bind=db_connection.engine)
     table = meta.tables[table_name]
